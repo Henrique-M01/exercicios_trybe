856b5ae6159d6fe5d2632e6d1974a1a95af8d080
henrique <hickm018@gmail.com>
2021-07-28 18:38:16 -0300

    Adiciona exercício 3

---
+++ fundamentos/bloco-4-intro-javascript/exercicios4.1/retorno>3.js
@@ -0,0 +1,13 @@
+let a = 22;
+let b = 9;
+let c = 18;
+
+if (a > b && a > c){
+    console.log("A é maior que B e C.")
+}
+else if (b > a && b > c){
+    console.log("B é maior que A e C.")
+}
+else {
+    console.log("C é maior que A e B.")
+}
