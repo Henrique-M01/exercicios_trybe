cabb74e29e66c2ee64fbd8d9092c5abcc47a5f12
henrique <hickm018@gmail.com>
2021-07-28 18:56:29 -0300

    Adiciona exercício 5

---
+++ fundamentos/bloco-4-intro-javascript/exercicios4.1/exercicio5.js
@@ -0,0 +1,16 @@
+let anguloA = 45;
+let anguloB = 57;
+let anguloC = 78;
+
+if (anguloA > 0 && anguloB > 0 && anguloC > 0){
+    let triang = anguloA + anguloB + anguloC;
+if (triang == 180){
+    console.log("true");
+}
+else {
+    console.log("false")
+}
+} else {
+    console.log("Números negativos são invalidos para angulos.")
+}
+
