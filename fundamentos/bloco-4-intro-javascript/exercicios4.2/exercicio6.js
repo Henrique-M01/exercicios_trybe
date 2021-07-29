79b34c3853f9046a74961e0319033990a58c4cdf
henrique <hickm018@gmail.com>
2021-07-29 18:23:07 -0300

    Adiciona exercicio 6

---
+++ fundamentos/bloco-4-intro-javascript/exercicios4.2/exercicio6.js
@@ -0,0 +1,12 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let impar = 0;
+
+for (let index = 0; index < numbers.length; index += 1){
+    if ((numbers[index] % 2) === 1){
+        impar++
+    }
+    else {
+        console.log("Nenhum valor ímpar encontrado.")
+    }
+}
+console.log(impar)
