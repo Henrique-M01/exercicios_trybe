bf03516385d28b7ab74d2a41a64d1cb1d851a1a8
henrique <hickm018@gmail.com>
2021-07-30 15:52:29 -0300

    Adiciona exercicio 1

---
+++ fundamentos/bloco-4-intro-javascript/exercicios4.3/exercicio1.js
@@ -0,0 +1,11 @@
+let n = 5;
+let symbols = '*';
+let linhasNum = '';
+
+for (let asterisco = 0; asterisco < n; asterisco += 1){
+    linhasNum += symbols
+}
+
+for (let index = 0; index < n; index += 1){
+    console.log(linhasNum)
+}
