d3f99ec186cb2f8e7719ba336f6e4ff0955d4a6e
henrique <hickm018@gmail.com>
2021-08-02 19:33:21 -0300

    Adiciona exercicios part 2.1

---
+++ fundamentos/bloco-4-intro-javascript/exercicios4.4/exe2.js
@@ -0,0 +1,34 @@
+// function verificaPalindrome(palavra){
+//     let palindromo = palavra.toString().split('').reverse().join('');
+//     if (palindromo === palavra){
+//         console.log(true)
+//     }
+//     else {
+//         console.log(false)
+//     }
+// }
+// verificaPalindrome('amor a roma')
+
+// let inteiros = [2, 3, 6, 7, 10, 1];
+// function retornoIndiceMaiorValor (inteiros){
+//     let maior = 0;
+//     for (let key = 0; key < 6; key += 1){
+//         if (inteiros[maior] < inteiros[key]){
+//             maior = key;
+//         }
+//     }
+//     return maior
+// }
+// console.log(retornoIndiceMaiorValor(inteiros))
+
+let inteiros = [2, 4, 6, 7, 10, 0, -3];
+function menorValor (inteiros){
+    let menor = 0;
+    for (let key in inteiros){
+        if (inteiros[menor] > inteiros[key]){
+            menor = key;
+        }
+    }
+    return menor;
+}
+console.log(menorValor(inteiros))
