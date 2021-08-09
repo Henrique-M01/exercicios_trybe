48f714de724d15822823e19f146c5d76401b9608
henrique <hickm018@gmail.com>
2021-08-09 13:38:35 -0300

    Adiciona exercicio 0, para fixar

---
+++ fundamentos/bloco-5-DOM-eventos-e-webStorage/exercicios-5-1/script.js
@@ -0,0 +1,31 @@
+const title = (document.getElementById(
+  "header-container"
+).style.backgroundColor = "green");
+
+let backUrgent = (document.getElementsByClassName(
+  "emergency-tasks"
+)[0].style.backgroundColor = "#deb887");
+
+let backNotUrgent = (document.getElementsByClassName(
+  "no-emergency-tasks"
+)[0].style.backgroundColor = "yellow");
+
+let trybeFooter = (document.getElementById(
+  "footer-container"
+).style.backgroundColor = "#006400");
+
+let titleUrgent1 = (document.querySelectorAll(
+  ".emergency-tasks div h3"
+)[1].style.backgroundColor = "#8b008b");
+
+let titleUrgent = (document.querySelectorAll(
+  ".emergency-tasks div h3"
+)[0].style.backgroundColor = "#8b008b");
+
+let titleNotUrgent1 = (document.querySelectorAll(
+  ".no-emergency-tasks div h3"
+)[0].style.backgroundColor = "black");
+
+let titleNotUrgent = (document.querySelectorAll(
+  ".no-emergency-tasks div h3"
+)[1].style.backgroundColor = "black");
