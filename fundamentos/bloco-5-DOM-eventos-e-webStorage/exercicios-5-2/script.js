25ee920828886ac3fe9cc88628aa2d5f03096185
henrique <hickm018@gmail.com>
2021-08-10 19:21:43 -0300

    Adiciona exdrcício 8

--- fundamentos/bloco-5-DOM-eventos-e-webStorage/exercicios-5-2/script.js
+++ fundamentos/bloco-5-DOM-eventos-e-webStorage/exercicios-5-2/script.js
@@ -26,3 +26,29 @@ main.appendChild(leftSquare);
 let rightSquare = document.createElement("section");
 rightSquare.className = "right-content";
 main.appendChild(rightSquare);
+
+let image = document.createElement("img");
+image.className = "small-image";
+image.src = "https://picsum.photos/200";
+leftSquare.appendChild(image);
+
+let list = document.createElement("ul");
+leftSquare.appendChild(list);
+let listItem = [
+  "um",
+  "dois",
+  "tres",
+  "quatro",
+  "cinco",
+  "seis",
+  "sete",
+  "oito",
+  "nove",
+  "dez",
+];
+for (let index = 0; index < 10; index += 1) {
+  let createList = document.createElement("li");
+  createList.innerHTML = listItem[index];
+  list.appendChild(createList);
+}
+console.log(listItem);
