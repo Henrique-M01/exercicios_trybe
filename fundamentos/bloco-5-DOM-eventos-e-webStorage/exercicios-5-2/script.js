93b221d7177d1e181e42b1fa6ea78cf051704170
henrique <hickm018@gmail.com>
2021-08-10 18:56:41 -0300

    Adiciona exerícios do 1 ao 3

---
+++ fundamentos/bloco-5-DOM-eventos-e-webStorage/exercicios-5-2/script.js
@@ -0,0 +1,16 @@
+let body = document.body;
+
+let h1 = document.createElement("h1");
+h1.innerText = "Exercício 5.2 - JavaScript DOM";
+
+body.appendChild(h1);
+
+let main = document.createElement("main");
+main.className = "main-content";
+
+body.appendChild(main);
+
+let sectionCenter = document.createElement("section");
+sectionCenter.className = "center-content";
+
+main.appendChild(sectionCenter);
