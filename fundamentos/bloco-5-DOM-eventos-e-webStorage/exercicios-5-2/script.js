02face3d4dd1edc15d4bbeec8ff9af99f9563285
henrique <hickm018@gmail.com>
2021-08-10 19:06:23 -0300

    Adiciona exercícios 4 ao 6

--- fundamentos/bloco-5-DOM-eventos-e-webStorage/exercicios-5-2/script.js
+++ fundamentos/bloco-5-DOM-eventos-e-webStorage/exercicios-5-2/script.js
@@ -14,3 +14,15 @@ let sectionCenter = document.createElement("section");
 sectionCenter.className = "center-content";
 
 main.appendChild(sectionCenter);
+
+let paragraph = document.createElement("p");
+paragraph.innerText = "Lorem ipsum carpe diem !!";
+sectionCenter.appendChild(paragraph);
+
+let leftSquare = document.createElement("section");
+leftSquare.className = "left-content";
+main.appendChild(leftSquare);
+
+let rightSquare = document.createElement("section");
+rightSquare.className = "right-content";
+main.appendChild(rightSquare);
