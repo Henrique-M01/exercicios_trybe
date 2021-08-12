442effc1525aa6d9fce6d634512fdbc629e72b7f
henrique <hickm018@gmail.com>
2021-08-12 11:28:34 -0300

    Adiciona exerício 1

--- fundamentos/bloco-5-DOM-eventos-e-webStorage/execicios-5-3/script.js
+++ fundamentos/bloco-5-DOM-eventos-e-webStorage/execicios-5-3/script.js
@@ -22,3 +22,33 @@ function createDaysOfTheWeek() {
 createDaysOfTheWeek();
 
 // Escreva seu código abaixo.
+const dezDaysList = [
+  29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+  21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31,
+];
+function createDays() {
+  let ul = document.getElementById("days");
+  for (let key = 0; key < dezDaysList.length; key += 1) {
+    let li = document.createElement("li");
+    li.innerText = dezDaysList[key];
+    li.classList.add("day");
+    ul.appendChild(li);
+    if (
+      dezDaysList[key] === 24 ||
+      dezDaysList[key] === 25 ||
+      dezDaysList[key] === 31
+    ) {
+      li.classList.add("holiday");
+    }
+    if (
+      dezDaysList[key] === 25 ||
+      dezDaysList[key] === 4 ||
+      dezDaysList[key] === 11 ||
+      dezDaysList[key] === 18
+    ) {
+      li.classList.add("friday");
+    }
+  }
+}
+
+createDays();
