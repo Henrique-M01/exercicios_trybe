ec9ef12143a466cc035f7de01feed8127decdb0f
henrique <hickm018@gmail.com>
2021-08-23 17:04:27 -0300

    Adiciona exercicios 6.2

--- fundamentos/bloco-6-forms-felxbox-responsivo/exercicios6.1/script.js
+++ fundamentos/bloco-6-forms-felxbox-responsivo/exercicios6.1/script.js
@@ -33,4 +33,13 @@ for (let key in estates) {
   const optionChieldSelected = document.createElement("option");
   let optionCreate = selectedEstate.appendChild(optionChieldSelected);
   optionCreate.innerText = estates[key];
+  optionCreate.value = estates[key];
 }
+
+// function stopFluxo(event) {
+//   event.preventDefault();
+//   console.log("ola");
+// }
+
+// const buttonSubmit = document.getElementById("button-submit");
+// buttonSubmit.addEventListener("click", stopFluxo);
