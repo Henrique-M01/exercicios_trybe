5520555f69132a0b209ef108b8a16427681c6045
henrique <hickm018@gmail.com>
2021-08-27 16:55:16 -0300

    Adiciona 1 exercicios part 1

---
+++ fundamentos/bloco-7-es6-testes-unitários/script.js
@@ -0,0 +1,18 @@
+// function testingScope(escopo) {
+//     if (escopo === true) {
+//       let ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
+//       ifScope = ifScope + ' ótimo, fui utilizada no escopo !';
+//       console.log(ifScope);
+//     } else {
+//       const elseScope = 'Não devo ser utilizada fora meu escopo (else)';
+//       console.log(elseScope);
+//     }
+//   }
+
+//   testingScope(true);
+
+const testingEscopo = escopo => 
+    (escopo === true) ? `Não devo ser utilizada fora do meu escopo (if) ótimo, fui utilizada no escopo !` 
+    : `Não devo ser utilizada fora meu escopo (else)`
+
+console.log(testingEscopo(true))
