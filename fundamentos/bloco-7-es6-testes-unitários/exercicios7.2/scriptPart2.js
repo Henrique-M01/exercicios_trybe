990a0ff60209bf45090774541d8ccf2772ab7ae7
henrique <hickm018@gmail.com>
2021-08-30 16:14:08 -0300

    Adiciona exercicio 1 part 2

---
+++ fundamentos/bloco-7-es6-testes-unitários/exercicios7.2/scriptPart2.js
@@ -0,0 +1,23 @@
+const lesson1 = {
+    materia: 'Matemática',
+    numeroEstudantes: 20,
+    professor: 'Maria Clara',
+    turno: 'manhã',
+  };
+  
+  const lesson2 = {
+    materia: 'História',
+    numeroEstudantes: 20,
+    professor: 'Carlos',
+  };
+  
+  const lesson3 = {
+    materia: 'Matemática',
+    numeroEstudantes: 10,
+    professor: 'Maria Clara',
+    turno: 'noite',
+  };
+let addMorningLesson2 = 
+(obj, keyAdd, keyValue) => obj[keyAdd] = keyValue; 
+addMorningLesson2(lesson2, 'turno', 'manhã');
+console.log(lesson2)
